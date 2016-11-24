8be100d73deeea077abdd157960ff79ac35c8294
Gavin Stark <gstark31897@gmail.com>
2016-11-24 17:51:52 -0600

    making a teapot

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 extern crate octengine;
 
-use octengine::{App};
+use octengine::*;
 
 fn main() {
     let app = App::new();
-    app.run();
+    let mut scene = Scene::empty();
+    
+    let teapot = Teapot::new(app.get_display());
+    let teapot_object = GameObject::new(Box::new(teapot));
+    scene.register_gameobject(teapot_object);
+
+    app.run(&scene);
 }
