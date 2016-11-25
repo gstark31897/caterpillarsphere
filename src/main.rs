82054fc0fbcf90b141b8373e4e6ddc314a9d4dfc
Gavin Stark <gstark31897@gmail.com>
2016-11-25 11:52:22 -0600

    working

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,13 @@ fn main() {
     let app = App::new();
     let mut scene = Scene::empty();
     
+    let teapot_function = | x: &mut f32, y: &mut f32, z: &mut f32, x_rot: &mut f32, y_rot: &mut f32, z_rot: &mut f32 | {
+        *y_rot = 5.0;
+    };
     let teapot = Teapot::new(app.get_display());
-    let teapot_object = GameObject::new(Box::new(teapot));
+    let mut teapot_object = GameObject::new(Box::new(teapot));
+    teapot_object.set_update_function(Box::new(teapot_function));
     scene.register_gameobject(teapot_object);
 
-    app.run(&scene);
+    app.run(&mut scene);
 }
